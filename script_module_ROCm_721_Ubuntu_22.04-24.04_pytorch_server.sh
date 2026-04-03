413ae3709a19a2727edc04e1650daa2c82b578fd
Jörg Roskowetz <141847198+JoergR75@users.noreply.github.com>
2026-04-03 10:16:55 +0200

    Update script_module_ROCm_721_Ubuntu_22.04-24.04_pytorch_server.sh

--- script_module_ROCm_721_Ubuntu_22.04-24.04_pytorch_server.sh
+++ script_module_ROCm_721_Ubuntu_22.04-24.04_pytorch_server.sh
@@ -30,7 +30,7 @@
 #
 # PyTorch:               2.12.0.dev20260307+rocm7.1
 # Transformers:          5.1.0
-# Docker:                29.2.1 min. 29.0.0 (the script will verify and skip installation if minimum requirements are installed)
+# Docker:                29.3.1 min. 29.0.0 (the script will verify and skip installation if minimum requirements are installed)
 #
 # INCLUDED TOOLS:
 # ---------------------------------------------------------------------------------------------------------------
